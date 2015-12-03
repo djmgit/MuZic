f8ffc2f0fe8415526ca760e539a31eee84a1723c
Rudra Nil Basu <rudra.nil.basu.1996@gmail.com>
2015-12-03 16:51:03 +0530

    Reading all Songs from the Device

--- MuZic/app/src/main/java/com/prizm/muzic/MainActivity.java
+++ MuZic/app/src/main/java/com/prizm/muzic/MainActivity.java
@@ -2,37 +2,67 @@ package com.prizm.muzic;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
 
+    ListView lv;
+    String[] items;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        lv=(ListView)findViewById(R.id.lvPlayList);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        ArrayList<File> mySongs = findSongs(Environment.getExternalStorageDirectory());
+        items = new String[mySongs.size()];
+        for (int i=0;i<mySongs.size();i++)
+        {
+            //toast(mySongs.get(i).getName().toString());
+            items[i]=mySongs.get(i).getName().toString().replace(".mp3","").replace(".mp3","");
         }
-
-        return super.onOptionsItemSelected(item);
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(getApplicationContext(), R.layout.song_layout,R.id.textView,items);
+        lv.setAdapter(adp);
     }
+
+    public ArrayList<File> findSongs(File root)
+    {
+        File [] files = root.listFiles();
+        ArrayList<File> al = new ArrayList<File>();
+        for(File singleFile : files)
+        {
+            if(singleFile.isDirectory()&& !singleFile.isHidden())
+            {
+                al.addAll(findSongs(singleFile));
+            }
+            else
+            {
+                if(singleFile.getName().endsWith(".mp3") || singleFile.getName().endsWith(".wav"))
+                {
+                    al.add(singleFile);
+                }
+            }
+        }
+        return  al;
+    }
+
+    public  void toast(String text)
+    {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
 }
